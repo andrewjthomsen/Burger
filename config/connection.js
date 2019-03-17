dd20eb0a857ece9dc2166ec63b736b0aab61c189
andrew <andrewjthomsen@yahoo.com>
2019-03-16 21:58:53 -0700

    Added mysql connection to connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 //Challenge level REQUIRED/ENTRY
-const inquirer = require("inquirer");
 const mysql = require("mysql");
 // connects to mysql
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Majesty88",
-  port: 3306,
-  database: "burger"
-});
+var connection;
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Majesty88",
+    port: 3306,
+    database: "burger"
+  });
+};
+
 // if connection fails, error prompt
 connection.connect(function (err) {
   if (err) {
