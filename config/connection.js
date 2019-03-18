27f407506f28365a2dcd4abec33a78d03be9b8ed
andrew <andrewjthomsen@yahoo.com>
2019-03-18 14:12:15 -0700

    Deployment changes made to connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const mysql = require("mysql");
 // connects to mysql
 var connection;
-if(process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
